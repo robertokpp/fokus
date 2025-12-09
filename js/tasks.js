16a1333c7231785bbd1ccb18e554e60714df386a
Roberto Kennedy <roberto.kpp@gmail.com>
2025-12-08 21:10:03 -0500

    acrescentando lista

--- js/tasks.js
+++ js/tasks.js
@@ -5,15 +5,32 @@ const btnAddTask = document.getElementById("btn-addtask");
 const btnDelete = document.getElementById("btn-delete");
 const btnCancel = document.getElementById("btn-cancel");
 const btnSave = document.getElementById("btn-save");
+const taskEdit = document.querySelectorAll(".task-edit")
 
 //Textarea
 const textTask = document.getElementById("text-task");
+const ul = document.querySelector("ul");
 
 const listTask = [];
+let numId = 1;
+
 
 function listCreation(task) {
   listTask.push(task);
-  console.log(listTask);
+  const li = document.createElement("li");
+  li.id = numId;
+  ul.append(li);
+  li.innerHTML = `<div>
+  <img src="assets/Ícones/Check_branco.svg" alt="" />
+  <p>${task}</p>
+  </div>
+  <img value="${numId}" src="assets/Ícones/edit.svg" alt="" />
+  `;
+  numId++;
+}
+
+function listEdit(task){
+  
 }
 
 btnAddTask.addEventListener("click", () => {
@@ -27,4 +44,9 @@ btnCancel.addEventListener("click", () => {
 btnSave.addEventListener("click", () => {
   listCreation(textTask.value);
   textTask.value = "";
+});
+
+
+taskEdit.addEventListener("click", () => {
+  listEdit();
 });
