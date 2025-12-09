80ae0c9b67a2143411965caa49c42a4dcbc4d8c7
Roberto Kennedy <roberto.kpp@gmail.com>
2025-12-09 17:50:40 -0500

    logica de tarefa

--- js/tasks.js
+++ js/tasks.js
@@ -5,11 +5,12 @@ const btnAddTask = document.getElementById("btn-addtask");
 const btnDelete = document.getElementById("btn-delete");
 const btnCancel = document.getElementById("btn-cancel");
 const btnSave = document.getElementById("btn-save");
-const taskEdit = document.querySelectorAll(".task-edit")
+const taskEdit = document.querySelectorAll(".task-edit");
 
 //Textarea
 const textTask = document.getElementById("text-task");
 const ul = document.querySelector("ul");
+const textEdit = document.querySelector(".task-edit");
 
 const listTask = [];
 let numId = 1;
@@ -24,12 +25,17 @@ function listCreation(task) {
   <img src="assets/Ícones/Check_branco.svg" alt="" />
   <p>${task}</p>
   </div>
-  <img value="${numId}" src="assets/Ícones/edit.svg" alt="" />
+  <img value="${numId}"class="task-edit" src="assets/Ícones/edit.svg" alt="" />
   `;
   numId++;
 }
 
-function listEdit(task){
+function renderDisplay(){
+
+}
+
+function listEdit(task) {
+  textTask.value = listTask[task];
   
 }
 
@@ -41,12 +47,22 @@ btnCancel.addEventListener("click", () => {
   addTask.style.display = "none";
 });
 
+
+// função que verifique se é editado ou novo
+// editado alterar o value da li
+// novo inclui no final da lista
 btnSave.addEventListener("click", () => {
   listCreation(textTask.value);
   textTask.value = "";
+  addTask.style.display = "none";
 });
 
-
-taskEdit.addEventListener("click", () => {
-  listEdit();
+ul.addEventListener("click", (event) => {
+  if (event.target.classList.contains("task-edit")) {
+    addTask.style.display = "flex";
+    const li = event.target.closest("li");
+    const id = li.id - 1
+    listEdit(id)
+  }
 });
+
