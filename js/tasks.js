a594cb9fdb0238a3c83a52eed20412c971f4a732
Roberto Kennedy <roberto.kpp@gmail.com>
2025-12-09 20:41:53 -0500

    corrigindo logica

--- js/tasks.js
+++ js/tasks.js
@@ -13,30 +13,36 @@ const ul = document.querySelector("ul");
 const textEdit = document.querySelector(".task-edit");
 
 const listTask = [];
-let numId = 1;
-
 
 function listCreation(task) {
   listTask.push(task);
-  const li = document.createElement("li");
-  li.id = numId;
-  ul.append(li);
-  li.innerHTML = `<div>
-  <img src="assets/Ícones/Check_branco.svg" alt="" />
-  <p>${task}</p>
-  </div>
-  <img value="${numId}"class="task-edit" src="assets/Ícones/edit.svg" alt="" />
-  `;
-  numId++;
+  renderDisplay();
 }
 
-function renderDisplay(){
-
+function renderDisplay() {
+  ul.innerHTML = "";
+  listTask.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.id = index;
+    ul.append(li);
+    li.innerHTML = `<div>
+  <img class="check-icons" src="assets/Ícones/Check_branco.svg" alt="" />
+  <p>${item}</p>
+  </div>
+  <img value="${index}"class="task-edit" src="assets/Ícones/edit.svg" alt="" />
+  `;
+  });
 }
 
 function listEdit(task) {
   textTask.value = listTask[task];
-  
+  listTask.splice(task, 1);
+  renderDisplay();
+}
+
+function listDelete() {
+  listTask.splice(task, 1);
+  renderDisplay();
 }
 
 btnAddTask.addEventListener("click", () => {
@@ -47,22 +53,30 @@ btnCancel.addEventListener("click", () => {
   addTask.style.display = "none";
 });
 
-
-// função que verifique se é editado ou novo
-// editado alterar o value da li
-// novo inclui no final da lista
 btnSave.addEventListener("click", () => {
   listCreation(textTask.value);
   textTask.value = "";
   addTask.style.display = "none";
 });
 
+btnDelete.addEventListener("click", () => {
+  textTask.value = ""
+});
+
+
 ul.addEventListener("click", (event) => {
   if (event.target.classList.contains("task-edit")) {
     addTask.style.display = "flex";
     const li = event.target.closest("li");
-    const id = li.id - 1
-    listEdit(id)
+    const id = li.id;
+    console.log(id);
+    listEdit(id);
   }
 });
 
+ul.addEventListener("click", (event) => {
+  if (event.target.classList.contains("check-icons")) {
+    const li = event.target.closest("li");
+    li.classList.add("task-completed");
+  }
+});
